ceae63a116e28d50aa8905ac088a2d3309c0bd1c
LapizLTD <122625367+LapizLTD@users.noreply.github.com>
2023-01-14 17:05:26 +0330

    Clearified and optimized WHOIS.py
    Rewrite, Blacked, Optimized and...

--- WHOIS.py
+++ WHOIS.py
@@ -1,85 +1,86 @@
-import socket
-from colorama import Fore
 import os
 import sys
+import re
 import time
+import socket
 import colorama
-def whois():
-    colorama.init()
-    os.system("clear")
-    print("")
-    print( Fore.LIGHTRED_EX + """██╗    ██╗██╗  ██╗ ██████╗ ██╗███████╗
+
+colorama.init()
+
+
+def print_n_sleep(to_be_printed: str, time_to_sleep: int = 0.3) -> None:
+    for meow in to_be_printed.split("\n"):
+        print(meow)
+        time.sleep(time_to_sleep if meow else 0)
+
+
+domain_extract_regex_pattern = r"(https://)?([A-Za-z0-9]+(\.[A-Za-z0-9]+)+)/?"
+
+banner = f"""{colorama.Fore.LIGHTRED_EX}
+██╗    ██╗██╗  ██╗ ██████╗ ██╗███████╗
 ██║    ██║██║  ██║██╔═══██╗██║██╔════╝
 ██║ █╗ ██║███████║██║   ██║██║███████╗
 ██║███╗██║██╔══██║██║   ██║██║╚════██║
 ╚███╔███╔╝██║  ██║╚██████╔╝██║███████║
  ╚══╝╚══╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝╚══════╝
-                                      """)
-    print("")
-    time.sleep(0.3)
-    print(Fore.LIGHTGREEN_EX + "[1] WHOIS start...")
-    print("")
-    time.sleep(0.3)
-    print(Fore.LIGHTYELLOW_EX + "[2] help...")
-    print("")
-    time.sleep(0.3)
-    print(Fore.LIGHTRED_EX + "[3] Exit...")
-    print("")
-    print(Fore.LIGHTYELLOW_EX + "[+]" + Fore.LIGHTGREEN_EX + "create by @Tganga36")
-    print("")
-    x = input(">>> ")
-    #----------------------------------------
 
-    if x == "1":
-        os.system("clear")
-        print("")
-        time.sleep(0.3)
-        domain = input("Domain : ").lower()
+{colorama.Fore.LIGHTGREEN_EX} [1] Start whois
+{colorama.Fore.LIGHTYELLOW_EX} [2] Help
+{colorama.Fore.LIGHTRED_EX} [3] Exit
 
-        domain = domain.replace("http://","")
-        domain = domain.replace("https://","")
-        domain = domain.replace("www.","")
+{colorama.Fore.LIGHTYELLOW_EX} [+]{colorama.Fore.LIGHTGREEN_EX} Coded by @Tganga36"""
 
-        if domain[-3:] == "org" or domain[-3:] == "com" or domain[-3:] == "net":
-            whois_server = "whois.internic.net"
-        else:
-            whois_server =  "whois.iana.org"
+while True:
+    os.system("clear")
+    print_n_sleep(banner)
+    try:
+        selected_option = int(input(">>> "))
+        match selected_option:
+            case 1:
+                os.system("clear")
+                print_n_sleep("")
+                domain = input("Enter domain: ").lower()
+                try:
+                    base = re.search(domain_extract_regex_pattern, domain)
+                    full_domain = base.group(2)
+                    ext = base.group(3)
 
-        s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+                    if ext in (".com", ".net", ".org"):
+                        whois_server = "whois.internic.net"
+                    else:
+                        whois_server = "whois.iana.org"
 
-        s.connect((whois_server,43))
+                    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                        sock.connect((whois_server, 43))
+                        sock.send(f"{full_domain}\r\n".encode())
+                        whois_result = ""
+                        while 1:
+                            data = sock.recv(1024)
+                            if data:
+                                whois_result += data.decode()
+                            else:
+                                break
+                    print_n_sleep(whois_result)
 
-        s.send((domain+"\r\n").encode())
-
-        msg = s.recv(10000)
-
-        print(msg.decode())
-        we = input("again :")
-        if we == "yes" :
-            whois()
-        else :
-            os.system("clear")
-    
-#............................................
-
-    if x == "2":
-        print(Fore.LIGHTYELLOW_EX + """ >>> In the domain field, enter the address of your desired site .... """)
-        print("")
-        qw = input( " Did you understand ? ")
-        if qw == "yes":
-            whois()
-        else :
-            print("")
-            print(Fore.LIGHTYELLOW_EX + """ >>> In the domain field, enter the address of your desired site .... """)
-            time.sleep(5)
-            whois()
-    if x == "3":
-        if x == "3":
-            print( Fore.LIGHTRED_EX + "see you Later ;) ")
-            time.sleep(0.2)
-            
-
-
-
-whois()
-    
+                    if input("Do you want to get whois again? (y/n) ").lower() == "n":
+                        break
+                    else:
+                        continue
+                except AttributeError:
+                    print("attr error ! (regex)")
+            case 2:
+                print_n_sleep(
+                    f"{colorama.Fore.LIGHTYELLOW_EX}>>> Enter the address of website in domain input."
+                )
+                if input("Did you understand? (y/n) ").lower() == "n":
+                    os.system("clear")
+                    print_n_sleep(
+                        f"{colorama.Fore.LIGHTYELLOW_EX}>>> Enter the address of website in domain input.",
+                        3,
+                    )
+                continue
+            case 3:
+                print_n_sleep(f"{colorama.Fore.LIGHTRED_EX}See you later XD")
+                break
+    except ValueError:
+        print("Invalid option !")
